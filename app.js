c52657b0cf0538c04dba9a52bd797cfa14c3fc6b
Jeremy Greer <jeremygreer@reergymerej.com>
2014-05-12 08:29:57 -0400

    add a couple examples from the end of ch4

---
+++ app.js
@@ -0,0 +1,5 @@
+var recursion = require('./ch4/recursion'),
+	scope = require('./ch4/scope');
+
+// recursion.factorial(4);
+scope.foo();
---
+++ ch4/recursion.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var factorial = function factorial(i, a) {
+	a = a || 1;
+	if (i < 2) {
+		return a;
+	}
+	return factorial(i - 1, a * i);
+};
+
+exports.factorial = factorial;
